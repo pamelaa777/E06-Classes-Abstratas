d68c02bc5cf09d2e6cde390791a6e174748c8347
Pâmela Eduarda <132526019+pamelaa777@users.noreply.github.com>
2024-06-22 13:33:26 -0300

    Update OperacaoDeposito.java

--- src/OperacaoDeposito.java
+++ src/OperacaoDeposito.java
@@ -1,12 +1,6 @@
 public class OperacaoDeposito extends Operacao {
+    
     public OperacaoDeposito(double valor) {
         super('d', valor);
-        setTipo('d');
-        setValor(valor);
-
-    }
-    @Override
-    public String toString(){
-        return "Deposito" + this.getValor();
     }
 }
