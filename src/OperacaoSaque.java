83b65045aceb136b2631eb0715b9a77c399b202f
Pâmela Eduarda <132526019+pamelaa777@users.noreply.github.com>
2024-06-22 13:34:40 -0300

    Update OperacaoSaque.java

--- src/OperacaoSaque.java
+++ src/OperacaoSaque.java
@@ -1,11 +1,6 @@
 public class OperacaoSaque extends Operacao {
+    
     public OperacaoSaque(double valor) {
-        super('s', valor);
-        setTipo('s');
-        setValor(valor);
-    }
-    @Override
-    public String toString(){
-        return "Saque" + this.getValor();
+        super('s', valor); 
     }
 }
