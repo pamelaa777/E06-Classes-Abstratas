da6a9cff2efc4ba0d6f0b403463566ff78e1c5ab
Pâmela Eduarda <132526019+pamelaa777@users.noreply.github.com>
2024-06-22 13:21:01 -0300

    Update Conta.java

--- src/Conta.java
+++ src/Conta.java
@@ -1,27 +1,21 @@
+import Operacao.*;
+import Cliente.*;
+import java.util.Arrays;
+
 public abstract class Conta {
 
-    private int numero;
+    protected int numero;
+    protected Cliente dono;
+    protected double saldo;
+    protected double limiteMax, limiteMin;
+    protected Operacao[] operacoes;
+    protected int proximaOperacao;
+    protected static int totalContas = 0;
 
-    private Cliente dono;
-
-    private double saldo;
-
-    protected double limite;
-
-    private Operacao[] operacoes;
-
-    private int proximaOperacao;
-
-    private static int totalContas = 0;
-
-    public abstract void setLimite(double limite);
-
-
-    public Conta(int numero, Cliente dono, double saldo, double limite) {
+    public Conta(int numero, Cliente dono, double saldo) {
         this.numero = numero;
         this.dono = dono;
         this.saldo = saldo;
-        this.limite = limite;
 
         this.operacoes = new Operacao[10];
         this.proximaOperacao = 0;
@@ -29,27 +23,19 @@ public abstract class Conta {
         Conta.totalContas++;
     }
 
-    private void redimensionarVetor() {
-        Operacao[] newArray = new Operacao[this.operacoes.length * 2];
-        for (int i = 0; i < this.operacoes.length; i++) {
-            newArray[i] = this.operacoes[i];
+    protected void redimensionarOperacoes() {
+        if (operacoes.length == proximaOperacao) {
+            operacoes = Arrays.copyOf(operacoes, operacoes.length * 2);
         }
-        this.operacoes = newArray;
     }
-
-    private void adicionarOperacao(Operacao operacao) {
-        if(this.proximaOperacao == this.operacoes.length) {
-            redimensionarVetor();
-        }
-        this.operacoes[this.proximaOperacao] = operacao;
-        this.proximaOperacao++;
-    }
-
+    
     public boolean sacar(double valor) {
-        if (valor >= 0 && valor <= this.limite) {
+        if (valor >= limiteMin && valor <= limiteMax) {
             this.saldo -= valor;
 
-            this.adicionarOperacao(new OperacaoSaque(valor));
+            this.operacoes[proximaOperacao] = new OperacaoSaque(valor);
+            this.proximaOperacao++;
+            redimensionarOperacoes();
             return true;
         }
 
@@ -58,7 +44,10 @@ public abstract class Conta {
 
     public void depositar(double valor) {
         this.saldo += valor;
-        this.adicionarOperacao(new OperacaoDeposito(valor));
+
+        this.operacoes[proximaOperacao] = new OperacaoDeposito(valor);
+        this.proximaOperacao++;
+        redimensionarOperacoes();
     }
 
     public boolean transferir(Conta destino, double valor) {
@@ -70,56 +59,37 @@ public abstract class Conta {
         return false;
     }
 
+    public String toString() {
+        return
+        "\n===== Conta " + this.numero + " ====="+
+        "\nDono: " + this.dono.getNome()+
+        "\nSaldo: " + this.saldo+
+        "\nLimite: " + this.limiteMax+
+        "\n====================";
+    }
 
+    @Override
+    public boolean equals(Object conta) {
+        return this.numero==((Conta) conta).numero; 
+    }
 
     public void imprimirExtrato() {
-        System.out.println("======= Extrato Conta " + this.numero + "======");
+        System.out.println("\n======= Extrato Conta " + this.numero + "======");
         for(Operacao atual : this.operacoes) {
             if (atual != null) {
-                atual.imprimir();
+                System.out.println(atual);
             }
         }
         System.out.println("====================");
     }
 
-    public int getNumero() {
-        return numero;
-    }
-
-    public Cliente getDono() {
-        return dono;
-    }
-
-    public double getSaldo() {
-        return saldo;
-    }
-
-    public double getLimite() {
-        return limite;
-    }
-
-    public static int getTotalContas() {
-        return Conta.totalContas;
-    }
-
-    public void setNumero(int numero) {
-        this.numero = numero;
-    }
-
-    public void setDono(Cliente dono) {
-        this.dono = dono;
-    }
-
-        @Override
-        public String toString() {
-            return "Numero: " + this.numero +
-                    "\nDono: " + this.dono +
-                    "\nSaldo: " + this.saldo +
-                    "\nLimite: " + this.limite;
-        }
-        @Override
-        public boolean equals(Object obj) {
-            Conta that = (Conta) obj;
-            return this.numero == that.numero;
-        }
-    }
+    public int getNumero() {return numero;}
+    public Cliente getDono() {return dono;}
+    public double getSaldo() {return saldo;}
+    public double getLimiteMax() {return limiteMax;}
+    public double getLimiteMin() {return limiteMin;}
+    public static int getTotalContas() {return Conta.totalContas;}
+    public void setNumero(int numero) {this.numero = numero;}
+    public void setDono(Cliente dono) {this.dono = dono;}
+    abstract void setLimite(double limiteMax, double limiteMin);
+}
