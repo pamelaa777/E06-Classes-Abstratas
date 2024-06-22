f080b91a21a2a877af2b5a1826da4f279fe900aa
Pâmela Eduarda <132526019+pamelaa777@users.noreply.github.com>
2024-06-22 13:51:09 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,70 +1,56 @@
+import Cliente.*;
+import Contas.*;
+import Operacao.*;
 public class Main {
     public static void main(String[] args) {
 
-        // Para gerar um CPF válido, utilize o seguinte link:
-        // https://www.4devs.com.br/gerador_de_cpf
-        PessoaFisica joao = new PessoaFisica(
-                "João",
+        PessoaFisica joao= new PessoaFisica("João",
                 "Av. Antonio Carlos. 6627",
                 "324.310.130-15",
                 36,
-                'm'
-        );
-
-        PessoaFisica junia = new PessoaFisica(
-                "João",
-                "Av. Antonio Carlos. 6627",
-                "324.310.130-15",
+                'm');
+        PessoaFisica junia = new PessoaFisica("Junia",
+                "Av. Antonio Carlos. 7777",
+                "324.310.130-16",
                 36,
-                'm'
-        );
+                'f');
 
-        System.out.println(joao.toString());
-        System.out.println(joao.equals(junia));
-
-        PessoaJuridica loja = new PessoaJuridica(
-                "Brecho",
+        PessoaJuridica loja1 = new PessoaJuridica("Brecho",
                 "Rua Dos Palmares 111, Planalto",
                 "123.124.124.0001.23",
                 16,
-                "financeiro"
-        );
-        PessoaJuridica loja2 = new PessoaJuridica(
-                "Casinha",
+                "financeiro");
+        PessoaJuridica loja2 = new PessoaJuridica( "Casinha",
                 "Rua Dos Palmares 222, Planalto",
                 "123.124.124.0001.23",
                 16,
-                "financeiro"
-        );
-        System.out.println(loja.toString());
-        System.out.println(loja.equals(loja2));
+                "financeiro");
 
-        if (joao != null) {
-            Conta conta1 = new ContaUniversitaria(1234, joao, 0, -200);
-            Conta conta2 = new ContaCorrente(12121, joao, 10000, 1500);
-            Conta conta3 = new ContaCorrente(12134, joao, 500, 1500);
-            conta2.setLimite(-300);
-            conta3.setLimite(300);
-            conta1.depositar(2000);
-            conta1.depositar(2000);
-            conta1.depositar(2000);
-            conta1.depositar(2000);
-            conta1.sacar(500);
+        Conta conta1 = new ContaUniversitaria(1234, joao, 565, 5888, 0);
+        Conta conta2 = new ContaCorrente(1234, junia, 0, 1500, 0);
 
-            conta2.transferir(conta1, 666);
-            conta2.depositar(234);
+        System.out.println(conta1);
 
-            conta1.imprimirExtrato();
-            conta2.imprimirExtrato();
+        conta1.depositar(2000);
+        conta1.sacar(500);
 
-            int media = Operacao.getTotalOperacoes() / Conta.getTotalContas();
-            System.out.println("Média de operações por conta aberta: " + media);
-        } else {
-            System.out.println("CPF Inválido!");
-        }
+        conta2.transferir(conta1, 666);
+        conta2.depositar(234);
 
-        String x = "23";
-        System.out.println(x=="23");
+        System.out.println(conta1);
+        conta1.imprimirExtrato();
+
+        System.out.println(junia);
+        System.out.println(loja1);
+
+        int media = Operacao.getTotalOperacoes() / Conta.getTotalContas();
+        System.out.println("Média de operações por conta aberta: " + media);
+
+        System.out.println("conta1 == conta2 ? " + conta1.equals(conta2));
+        System.out.println("junia == joao ? " + junia.equals(joao));
+        System.out.println("junia == loja1 ? " + loja1.equals(junia));
+
+        System.out.println("authenticator: " + loja1.autenticar("555669813"));
 
     }
 }
