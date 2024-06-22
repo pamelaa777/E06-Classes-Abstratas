c1362f2b39276721fc95e0a4402bb398d7bfc214
Pâmela Eduarda <132526019+pamelaa777@users.noreply.github.com>
2024-06-22 13:31:42 -0300

    Update Operacao.java

--- src/Operacao.java
+++ src/Operacao.java
@@ -1,15 +1,10 @@
 import java.util.Date;
 
-public class Operacao {
+public abstract class Operacao {
 
-    /* Data de realização da operação */
     private Date data;
-
-    /* Tipo da operação */
-    private char tipo;
-
-    /* Valor da operação */
-    private double valor;
+    protected char tipo;
+    protected double valor;
 
     private static int totalOperacoes = 0;
 
@@ -21,32 +16,15 @@ public class Operacao {
         Operacao.totalOperacoes++;
     }
 
-    void imprimir() {
-        System.out.printf("%s\t%s\t%s\n", this.data, this.tipo, this.valor);
+    @Override
+    public String toString() {
+        return String.format("%s\t%s\t%s", this.data,  this.tipo,  this.valor);
     }
 
-    public Date getData() {
-        return data;
-    }
-
-    public char getTipo() {
-        return tipo;
-    }
-
-    public double getValor() {
-        return valor;
-    }
-
-    public static int getTotalOperacoes() {
-        return Operacao.totalOperacoes;
-    }
-
-    public void setTipo(char tipo) {
-        if(tipo == 'd' || tipo == 's')
-            this.tipo = tipo;
-    }
-
-    public void setValor(double valor) {
-        this.valor = valor;
-    }
+    public Date getData() {return data;}
+    public char getTipo() {return tipo;}
+    public double getValor() {return valor;}
+    public static int getTotalOperacoes() {return Operacao.totalOperacoes; }
+    public void setTipo(char tipo) {if(tipo == 'd' || tipo == 's')this.tipo = tipo;}
+    public void setValor(double valor) {this.valor = valor;}
 }
